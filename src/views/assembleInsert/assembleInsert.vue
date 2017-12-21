3cd674109b6ae0b493c9f634537e87877dc1d1f8
cn6696826 <544868267@qq.com>
2017-12-21 15:41:36 +0800

    progress

--- src/views/assembleInsert/assembleInsert.vue
+++ src/views/assembleInsert/assembleInsert.vue
@@ -1,11 +1,32 @@
 <template>
-  <div class="">
+  <div class="assembleInsert">
+    <convent></convent>
+    <div class="topbox">
+      <el-container>
+        <el-aside width="50%">
+          <proinfo></proinfo>
+
+        </el-aside>
+        <el-main>
+          <pro-gress></pro-gress>
+        </el-main>
+      </el-container>
+    </div>
+    <div class="bottom-box">
+
+    </div>
   </div>
 </template>
 
 <script type="text/babel">
-
+  export  default {
+    data(){
+      return{
+        tableData:[]
+      }
+    }
+  }
 </script>
 <style lang="less" scoped>
-
+  @import "../../css/assembleInsert.less";
 </style>
