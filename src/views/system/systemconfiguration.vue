b26c14c9bd64996a047798b6455c09eb5ba80bf8
zhengdong.yang <zhengdong.yang@geely.com>
2017-12-26 15:52:50 +0800

    port configure

--- src/views/system/systemconfiguration.vue
+++ src/views/system/systemconfiguration.vue
@@ -2,85 +2,81 @@
   <div class="systemConfiguration">
     <el-container>
       <el-header>
-        出口设置
+        串口设置
       </el-header>
       <el-main>
-        <div class="main-content">
+        <el-form ref="form" :model="form">
           <el-row>
             <el-col :span="12">
-              <div class="grid-content bg-purple">
-                <span>端口：</span>
-                <template>
-                  <el-select v-model="value" placeholder="请选择">
-                    <el-option
-                      v-for="item in options"
-                      :key="item.value"
-                      :label="item.label"
-                      :value="item.value">
-                    </el-option>
-                  </el-select>
-                </template>
-              </div>
+              <el-form-item label="端口">
+                <el-select v-model="form.port" placeholder="请选择端口">
+                  <el-option label="COM1" value="COM1"></el-option>
+                  <el-option label="COM2" value="COM2"></el-option>
+                  <el-option label="COM3" value="COM3"></el-option>
+                  <el-option label="COM4" value="COM4"></el-option>
+                  <el-option label="COM5" value="COM5"></el-option>
+                  <el-option label="COM6" value="COM6"></el-option>
+                </el-select>
+              </el-form-item>
             </el-col>
             <el-col :span="12">
-              <div class="grid-content bg-purple-light">
-                <span>波特率：</span>
-                <template>
-                  <el-select v-model="value" placeholder="请选择">
-                    <el-option
-                      v-for="item in options"
-                      :key="item.value"
-                      :label="item.label"
-                      :value="item.value">
-                    </el-option>
-                  </el-select>
-                </template>
-              </div>
+              <el-form-item label="波特率">
+                <el-select v-model="form.baudRate" placeholder="请选择波特率">
+                  <el-option label="300" value="300"></el-option>
+                  <el-option label="600" value="600"></el-option>
+                  <el-option label="1200" value="1200"></el-option>
+                  <el-option label="2400" value="2400"></el-option>
+                  <el-option label="4800" value="4800"></el-option>
+                  <el-option label="9600" value="9600"></el-option>
+                  <el-option label="19200" value="19200"></el-option>
+                  <el-option label="38400" value="38400"></el-option>
+                  <el-option label="43000" value="43000"></el-option>
+                  <el-option label="56000" value="56000"></el-option>
+                  <el-option label="57600" value="57600"></el-option>
+                  <el-option label="115200" value="115200"></el-option>
+                </el-select>
+              </el-form-item>
             </el-col>
           </el-row>
           <el-row>
             <el-col :span="12">
-              <div class="grid-content bg-purple">
-                <span>数据位：</span>
-                <template>
-                  <el-select v-model="value" placeholder="请选择">
-                    <el-option
-                      v-for="item in options"
-                      :key="item.value"
-                      :label="item.label"
-                      :value="item.value">
-                    </el-option>
-                  </el-select>
-                </template>
-              </div>
+              <el-form-item label="数据位">
+                <el-input v-model="form.dataBit" placeholder="请输入数据位"></el-input>
+              </el-form-item>
             </el-col>
             <el-col :span="12">
-              <div class="grid-content bg-purple-light">
-                <span>是否启用：</span>
-                <template>
-                  <el-select v-model="value" placeholder="请选择">
-                    <el-option
-                      v-for="item in options"
-                      :key="item.value"
-                      :label="item.label"
-                      :value="item.value">
-                    </el-option>
-                  </el-select>
-                </template>
-              </div>
+              <el-form-item label="校验位">
+                <el-select v-model="form.checkBit" placeholder="请选择校验位">
+                  <el-option label="None" value="None"></el-option>
+                  <el-option label="None" value="None"></el-option>
+                </el-select>
+              </el-form-item>
             </el-col>
           </el-row>
           <el-row>
-            <el-col :span="24">
-              <div class="grid-content bg-purple-dark">
-                <el-button type="primary">确定</el-button>
-                <el-button type="success">返回</el-button>
-              </div>
+            <el-col :span="12">
+              <el-form-item label="停止位">
+                <el-select v-model="form.stopBit" placeholder="请选择停止位">
+                  <el-option label="1" value="1"></el-option>
+                  <el-option label="2" value="2"></el-option>
+                </el-select>
+              </el-form-item>
+            </el-col>
+            <el-col :span="12">
+              <el-form-item label="是否生效">
+                <el-select v-model="form.enable" placeholder="是否生效">
+                  <el-option label="是" value="是"></el-option>
+                  <el-option label="否" value="否"></el-option>
+                </el-select>
+              </el-form-item>
             </el-col>
           </el-row>
-        </div>
+          <el-form-item>
+            <el-button type="primary" @click="onSubmit">保存</el-button>
+            <el-button>取消</el-button>
+          </el-form-item>
+        </el-form>
       </el-main>
-      <el-footer></el-footer>
     </el-container>
   </div>
 </template>
@@ -89,31 +85,30 @@
   export default {
     data() {
       return {
-        options: [{
-          value: '选项1',
-          label: '黄金糕'
-        }, {
-          value: '选项2',
-          label: '双皮奶'
-        }, {
-          value: '选项3',
-          label: '蚵仔煎'
-        }, {
-          value: '选项4',
-          label: '龙须面'
-        }, {
-          value: '选项5',
-          label: '北京烤鸭'
-        }],
-        value: ''
+        form: {
+          port: '',
+          baudRate: '',
+          dataBit: '',
+          checkBit: '',
+          stopBit: '',
+          enable: '',
+          terminalCode:'222'
+        }
+
       }
     },
     methods: {
-//        saleSave(status){
-//          this.$httpUtil.post('/biz/contract?status='+status,{formId:'contractSaleAdd',validateType:validateType}).then((response)=>{
-//            this.$router.replace({name:'全部合同订单',params:{contractType:'01'}});
-//          });
-//        },
+      onSubmit() {
+
+        var formdata = JSON.stringify(this.form);
+
+        this.$httpUtil.post('restful/cm/saveTerminalunit ', formdata).then((response) => {
+          // success callback
+
+        }, (response) => {
+
+        });
+      }
     }
   }
 </script>
