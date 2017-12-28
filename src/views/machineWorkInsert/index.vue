085947dd64cb26cfbbcc5e69339bab4d9be417c2
cn6696826 <544868267@qq.com>
2017-12-28 14:09:10 +0800

    机加下线表格

--- src/views/machineWorkInsert/index.vue
+++ src/views/machineWorkInsert/index.vue
@@ -1,6 +1,41 @@
 <template>
   <div class="macheineWorkInsert">
     <convent></convent>
+    <div class="bottom-form" style="margin-top: 1rem;">
+      <el-table
+        :data="tableData"
+        border
+        style="width: 100%">
+        <el-table-column
+          prop="date"
+          label="序号"
+          width="60">
+        </el-table-column>
+        <el-table-column
+          prop="name"
+          label="订单编号"
+          width="180">
+        </el-table-column>
+        <el-table-column
+          prop="address"
+          label="物料编号">
+        </el-table-column>
+        <el-table-column
+          prop="address"
+          width="240"
+          label="产品序列号">
+        </el-table-column>
+        <el-table-column
+          prop="address"
+          label="数量">
+        </el-table-column>
+        <el-table-column
+          prop="address"
+          label="备注">
+        </el-table-column>
+
+      </el-table>
+    </div>
   </div>
 </template>
 
