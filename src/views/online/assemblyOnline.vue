4b4ac66e8116b2459b507127edd331ecdc1b52e3
zhengdong.yang <zhengdong.yang@geely.com>
2018-01-03 17:25:10 +0800

    assemblyonline queue

--- src/views/online/assemblyOnline.vue
+++ src/views/online/assemblyOnline.vue
@@ -12,42 +12,80 @@
       </div>
     </div>
     <div class="bottom">
-      <table>
-        <thead>
-        <tr>
-          <td>序号</td>
-          <td>订单编号</td>
-          <td>工单编号</td>
-          <td>物料编码</td>
-          <td>物料描述</td>
-          <td>计划数量</td>
-          <td>机型</td>
-          <td>顺序号</td>
-        </tr>
-        </thead>
-        <tbody>
-        <tr>
-          <td></td>
-          <td></td>
-          <td></td>
-          <td></td>
-          <td></td>
-          <td></td>
-          <td></td>
-          <td></td>
-        </tr>
-        </tbody>
-      </table>
+      <el-table :data="tableData" stripe="true" style="width: 100%;">
+        <el-table-column prop="productOrderNum" label="订单编号" width="">
+        </el-table-column>
+        <el-table-column prop="productionOrderNum" label="工单编号">
+        </el-table-column>
+        <el-table-column prop="productModel" label="机型">
+        </el-table-column>
+        <el-table-column prop="materialCode" label="物料编码">
+        </el-table-column>
+        <el-table-column prop="materialText" label="物料描述">
+        </el-table-column>
+        <el-table-column prop="plannedQty" label="计划数量">
+        </el-table-column>
+        <el-table-column prop="orderNo" label="顺序号">
+        </el-table-column>
+      </el-table>
     </div>
   </div>
 </template>
 
 <script>
   // import currentorder from './component/currentOrder.vue';
+  import util from '../../utils/util.js';
+  export default{
+  data() {
+    return {
+      tableData: [],
+    }
+  },
+    created() {
+      this.getData();
+    },
+    methods: {
+      getData: function () {
+
+        let loc = JSON.parse(window.localStorage.getItem('terminal'));
+        let body = {
+          siteCode: loc.siteCode,
+          workCenterCode: loc.workCenterCode,
+          statuseCode: '10',
+          endRow: 5
+        };
+        this.$http({
+          headers: {
+            'Content-Type': 'application/x-www-form-urlencoded',
+          },
+          method: 'get',
+          url: 'http://10.200.151.229:8021/pcs/restful/pp/getProcutionOrderQueue',
+          params: body
+        })
+          .then((response) => {
+            this.tableData = response.data.data;
+          })
+      }
+    }
+
+  }
 </script>
 
 
 <style lang="less" scoped>
+  .bottom {
+    /*height: calc(100% - 26rem);*/
+    height:30%;
+    background-color: #fff;
+    .el-table {
+      font-size: 1.2rem;
+      .el-table__header-wrapper {
+        color: #222;
+
+      }
+
+    }
+  }
   .bg {
     box-sizing: border-box;
     background: #ededed;
@@ -78,38 +116,38 @@
     background: white;
   }
 
-  .bottom thead {
-    background: #F9F9F9;
-  }
+  /*.bottom thead {*/
+    /*background: #F9F9F9;*/
+  /*}*/
 
-  .bottom tbody {
-    background: #FFFFFF;
-  }
+  /*.bottom tbody {*/
+    /*background: #FFFFFF;*/
+  /*}*/
 
-  .bottom {
-    height: 170px;
-    /*margin-top: 10px;*/
-    background: white;
-  }
+  /*.bottom {*/
+    /*height: 170px;*/
+    /*!*margin-top: 10px;*!*/
+    /*background: white;*/
+  /*}*/
 
-  .bottom table {
-    width: 100%;
-    color: #222222;
-  }
+  /*.bottom table {*/
+    /*width: 100%;*/
+    /*color: #222222;*/
+  /*}*/
 
-  .bottom table,
-  .bottom table tr,
-  .bottom table td {
-    border: 1px solid #F1F1F1;
-  }
+  /*.bottom table,*/
+  /*.bottom table tr,*/
+  /*.bottom table td {*/
+    /*border: 1px solid #F1F1F1;*/
+  /*}*/
 
-  .bottom tr {
-    height: 54px;
-  }
+  /*.bottom tr {*/
+    /*height: 54px;*/
+  /*}*/
 
-  .bottom td {
-    padding-left: 10px;
-  }
+  /*.bottom td {*/
+    /*padding-left: 10px;*/
+  /*}*/
 
   .engine {
   background: url("../../assets/engie.png") no-repeat center;
