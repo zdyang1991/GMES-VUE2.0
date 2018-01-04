ff07937a3ec497cbb7b09aa2705171f0442f0e6f
cn6696826 <544868267@qq.com>
2018-01-04 10:55:16 +0800

    字段修改

--- src/views/online/machineplusOnline.vue
+++ src/views/online/machineplusOnline.vue
@@ -1,57 +1,110 @@
 <template>
-    <div class="machineplusOnline">
-      <convent></convent>
-      <div class="topbox">
-      </div>
-      <div class="bottom-form">
-        <el-table
-          :data="tableData"
-          border
-          style="width: 100%">
-          <el-table-column
-            prop="date"
-            label="序号"
-            width="60">
-          </el-table-column>
-          <el-table-column
-            prop="name"
-            label="订单编号"
-            width="180">
-          </el-table-column>
-          <el-table-column
-            prop="address"
-            label="工单编号">
-          </el-table-column>
-          <el-table-column
-            prop="address"
-            label="机型">
-          </el-table-column>
-          <el-table-column
-            prop="address"
-            label="物料编码">
-          </el-table-column>
-          <el-table-column
-            prop="address"
-            label="物料描述">
-          </el-table-column>
-          <el-table-column
-            prop="address"
-            label="计划数量">
-          </el-table-column>
-          <el-table-column
-            prop="address"
-            label="顺序号">
-          </el-table-column>
-        </el-table>
+  <div class="machineplusOnline">
+    <convent></convent>
+    <div class="topbox">
+      <div id="currentorder" class="right-container">
+        <!-- 第一行 -->
+        <div class="row f-df">
+          <div class="row-container f-df">
+            <div class="item-container f-f1">
+              <label class="label">{{formName.workNum}}</label>
+              <div class="detail">222222112</div>
+            </div>
+            <div class="item-container f-f1">
+              <label class="label">{{formName.machineType}}</label>
+              <div class="detail"></div>
+            </div>
+          </div>
+        </div>
+        <!-- 第二行 -->
+        <div class="row f-df">
+          <div class="row-container f-df">
+            <div class="item-container f-f1">
+              <label class="label">{{formName.matterDNum}}</label>
+              <div class="detail"></div>
+            </div>
+            <div class="item-container f-f1">
+              <label class="label">{{formName.matterDepict}}</label>
+              <div class="detail"></div>
+            </div>
+          </div>
+        </div>
+        <!-- 第三行 -->
+        <div class="row f-df">
+          <div class="row-container f-df">
+            <!--<div class="item-container f-f1">-->
+            <!--<label class="label">计划数量</label>-->
+            <!--<div class="detail"></div>-->
+            <!--</div>-->
+            <div class="item-container f-f1">
+              <label class="label">{{formName.properNum}}</label>
+              <div class="detail">1111111111111111111111111</div>
+            </div>
+          </div>
+        </div>
+        <div class="row f-df">
+          <div class="row-container f-df">
+            <div class="item-container f-f1">
+              <label class="label">{{formName.motorNum}}</label>
+              <div class="detail"></div>
+            </div>
+            <div class="item-container f-f1">
+              <label class="label">{{formName.orderNum}}</label>
+              <div class="detail"></div>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
+    <div class="bottom-form">
+      <el-table
+        :data="tableData"
+        border
+        style="width: 100%">
+        <el-table-column
+          prop="date"
+          label="序号"
+          width="60">
+        </el-table-column>
+        <el-table-column
+          prop="name"
+          label="订单编号"
+          width="180">
+        </el-table-column>
+        <el-table-column
+          prop="address"
+          label="工单编号">
+        </el-table-column>
+        <el-table-column
+          prop="address"
+          label="机型">
+        </el-table-column>
+        <el-table-column
+          prop="address"
+          label="物料编码">
+        </el-table-column>
+        <el-table-column
+          prop="address"
+          label="物料描述">
+        </el-table-column>
+        <el-table-column
+          prop="address"
+          label="计划数量">
+        </el-table-column>
+        <el-table-column
+          prop="address"
+          label="顺序号">
+        </el-table-column>
+      </el-table>
+    </div>
+  </div>
 </template>
 
 <script type="text/babel">
   export default {
-    data(){
-      return{
-        tableData:[],
+    data() {
+      return {
+        tableData: [],
         formName: {
           workNum: "工单编号",
           machineType: '机型',
