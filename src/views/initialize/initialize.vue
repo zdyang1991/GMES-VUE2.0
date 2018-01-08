15f0359b7768dc7ffc9ec71c7f1e7f00f46bc541
zhengdong.yang <zhengdong.yang@geely.com>
2018-01-08 10:10:50 +0800

    remove proinfo

--- src/views/initialize/initialize.vue
+++ src/views/initialize/initialize.vue
@@ -4,8 +4,68 @@
     <div class="topbox">
       <el-container>
         <el-aside width="60%">
-          <proinfo :formName="formName"></proinfo>
+          <!--<proinfo :formName="formName"></proinfo>-->
+          <div>
+            <div id="currentorder" class="right-container">
+              <!-- 第一行 -->
+              <div class="row f-df">
+                <div class="row-container f-df">
+                  <div class="item-container f-f1">
+                    <!--订单编号-->
 
+                    <label class="label">工单编号</label>
+                    <div class="detail"></div>
+
+                  </div>
+                  <div class="item-container f-f1">
+                    <!--工单编号-->
+                    <label class="label">机型</label>
+                    <div class="detail"></div>
+                  </div>
+                </div>
+              </div>
+              <!-- 第二行 -->
+              <div class="row f-df">
+                <div class="row-container f-df">
+                  <div class="item-container f-f1">
+                    <!--发动机号-->
+                    <label class="label">物料编号</label>
+                    <div class="detail"></div>
+                  </div>
+                  <div class="item-container f-f1">
+                    <!--机型-->
+                    <label class="label">物料描述</label>
+                    <div class="detail"></div>
+                  </div>
+                </div>
+              </div>
+              <!-- 第三行 -->
+              <div class="row f-df">
+                <div class="row-container f-df">
+                  <div class="item-container f-f1">
+                  <label class="label">计划数量</label>
+                  <div class="detail"></div>
+                  </div>
+                  <div class="item-container f-f1">
+                    <label class="label">顺序号</label>
+                    <div class="detail"></div>
+                  </div>
+                </div>
+              </div>
+              <div class="row f-df">
+                <div class="row-container f-df">
+                  <div class="item-container f-f1">
+                    <label class="label">发动机号</label>
+                    <div class="detail"></div>
+                  </div>
+                  <div class="item-container f-f1">
+                    <label class="label">订单编号</label>
+                    <div class="detail"></div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
         </el-aside>
         <el-main>
           <pro-gress></pro-gress>
@@ -142,5 +202,183 @@
   }
   .el-dialog{
     height: 30rem;
+  }
+  .unequal {
+    height: 20%;
+    padding: 20px;
+    box-sizing: border-box;
+  }
+
+  .row {
+    flex: 1;
+    padding: 1rem;
+    padding-top: 0;
+    box-sizing: border-box;
+    margin-top: 0.5rem;
+  }
+
+  .row:first-child {
+    margin-top: 1rem;
+  }
+
+  .pictureRow {
+    flex: 2;
+    padding: 20px;
+    box-sizing: border-box;
+  }
+
+  .upperTable {
+    min-width: 1020px;
+  }
+
+  .right-container {
+    height: 100%;
+    display: flex;
+    flex-direction: column;
+  }
+
+  .row-container {
+    height: 65px;
+    width: 100%;
+  }
+
+  .pictureRow-container {
+    height: 100%;
+    width: 100%;
+  }
+
+  /* 从第二个子元素开始生效 */
+  /*.item-container{*/
+  /*margin-top: 0.2rem;*/
+  /*}*/
+
+  .item-container:nth-child(n+2) {
+    margin-left: 20px;
+  }
+
+  .label {
+    color: #595959;
+    font-size: 1.4rem;
+    line-height: 2rem;
+  }
+
+  .detail {
+    color: #222222;
+    font-weight: bolder;
+    font-size: 1.8rem;
+    border-bottom: 1px solid #CDCDCD;
+    line-height: 36px;
+    overflow: hidden;
+    text-overflow: ellipsis;
+    white-space: nowrap;
+    min-height: 36px;
+    margin-top: 0.8rem;
+  }
+
+  .remain-amount-box {
+    margin-left: 20px;
+  }
+
+  .remain-amount-container {
+    border: 1px dashed #EB6F83;
+    height: 98%;
+    display: flex;
+    /* line-height: 63px;  */
+  }
+
+  .remain-amount {
+    color: #E43246;
+    font-size: 48px;
+    font-weight: bold;
+    align-items: center;
+    display: flex;
+    max-width: 50%;
+    overflow: hidden;
+    text-overflow: ellipsis;
+    white-space: nowrap;
+    margin-left: 10px;
+  }
+
+  .remain-amount-label {
+    color: #222222;
+    display: flex;
+    padding-left: 20px;
+    font-weight: bold;
+    align-items: center;
+  }
+
+  .confirmOnline {
+    height: 100%;
+    width: 100%;
+    /* background: #00AFEB; */
+    /* border-radius: 0px; */
+    font-size: 20px;
+  }
+
+  .remark {
+    min-height: 36px;
+    line-height: 36px;
+    /* line-height: 21px; */
+    color: #222222;
+    font-weight: bolder;
+    font-size: 18px;
+    border-bottom: 1px solid #CDCDCD;
+    text-overflow: ellipsis;
+    word-wrap: break-word;
+    -webkit-line-clamp: 2;
+    display: -webkit-box;
+    -webkit-box-orient: vertical;
+    overflow: hidden;
+  }
+
+  .bottom thead {
+    height: 50px;
+    background: #F9F9F9;
+  }
+
+  .bottom tbody {
+    background: #FFFFFF;
+  }
+
+  .bottom {
+    height: 161px;
+    margin-top: 10px;
+    background: white;
+  }
+
+  .blink {
+    -webkit-animation: blink 1.5s 0s infinite;
+    animation: blink 1.5s 0s infinite;
+  }
+
+  @keyframes blink {
+    0% {
+      box-shadow: 0 0 10px #EB6F83 inset;
+    }
+    50% {
+      box-shadow: 0 0 40px #EB6F83 inset;
+    }
+    100% {
+      box-shadow: 0 0 10px #EB6F83 inset;
+    }
+  }
+
+  .bottom table {
+    width: 100%;
+    color: #222222;
+  }
+
+  .bottom table,
+  .bottom table tr,
+  .bottom table td {
+    border: 1px solid #F1F1F1;
+  }
+
+  .bottom tr {
+    height: 50px;
+  }
+
+  .bottom td {
+    padding-left: 10px;
   }
 </style>
