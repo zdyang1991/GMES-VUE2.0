7acb87e3e60df01d75571e108e8e6bc63d3051f3
cn6696826 <544868267@qq.com>
2018-01-09 15:03:12 +0800

    fullscreen

--- src/components/navlist/src/navlist.vue
+++ src/components/navlist/src/navlist.vue
@@ -8,18 +8,24 @@
         <span>{{userName}}</span>
       </div>
       <ul>
-        <li  v-for="(item,index) in MenuListData"  @click="getTitle(index)" ref='menuTitle'>
-          <router-link :to="{name:item.menuText}" tag="div"  >
+        <li v-for="(item,index) in MenuListData" @click="getTitle(index)" ref='menuTitle'>
+          <router-link :to="{name:item.menuText}" tag="div">
             {{item.menuText}}
           </router-link>
         </li>
       </ul>
-      <button class="bottom-con" >
-        <div v-on:click="quit()" style="display: flex">
+      <button class="bottom-con">
+        <div style="display: flex" @click="requestFullScreen()">
           <span class="el-icon-pad-off"></span>
-          退出登录
+          全屏
         </div>
       </button>
+      <!--<button class="bottom-con" >-->
+      <!--<div v-on:click="quit()" style="display: flex">-->
+      <!--<span class="el-icon-pad-off"></span>-->
+      <!--退出登录-->
+      <!--</div>-->
+      <!--</button>-->
     </div>
   </transition>
 
@@ -27,26 +33,28 @@
 <script type="text/babel">
   import util from '../../../utils/util.js';
   import config from '../../../js/config';
+
   export default {
     name: 'navlist',
-    props:['isMenuShow'],
-    userName:'',
-    computed:{
-      MenuListData(){
-        let obj=JSON.parse(localStorage.getItem("list"));
+    props: ['isMenuShow'],
+    fullscreen:false,
+    userName: '',
+    computed: {
+      MenuListData() {
+        let obj = JSON.parse(localStorage.getItem("list"));
         return obj
       }
     },
-    created(){
+    created() {
       this.userinfo();
     },
-    data(){
+    data() {
       return {
         showColor: true,
       };
     },
-    methods:{
-      quit(){
+    methods: {
+      quit() {
         this.$http({
             headers: {
               'Content-Type': 'application/x-www-form-urlencoded',
@@ -63,19 +71,47 @@
           })
 
       },
-      userinfo(){
-        this.userName=sessionStorage.getItem("userCode");
+      userinfo() {
+        this.userName = sessionStorage.getItem("userCode");
 
       },
-      getTitle(index){
+      getTitle(index) {
         let getMenuText = this.$refs.menuTitle[index].innerText;
         this.$store.state.title = getMenuText;
-          localStorage.setItem('title',getMenuText)
-      }
+        localStorage.setItem('title', getMenuText)
+      },
+      //进入全屏
+      requestFullScreen() {
+        var element = document.documentElement;
+        this.fullscreen=!this.fullscreen;
+        console.log(this.fullscreen)
+        if(this.fullscreen==true){
+          if(element.requestFullscreen) {
+            element.requestFullscreen();
+          } else if(element.mozRequestFullScreen) {
+            element.mozRequestFullScreen();
+          } else if(element.webkitRequestFullscreen) {
+            element.webkitRequestFullscreen();
+          } else if(element.msRequestFullscreen) {
+            element.msRequestFullscreen();
+          }
+
+        }else{
+          if(document.exitFullscreen) {
+            document.exitFullscreen();
+          } else if(document.mozCancelFullScreen) {
+            document.mozCancelFullScreen();
+          } else if(document.webkitExitFullscreen) {
+            document.webkitExitFullscreen();
+          }
+        }
+
+      },
+
 
     }
   }
 </script>
 <style lang="less" scoped>
-@import "../../../css/navlist.less";
+  @import "../../../css/navlist.less";
 </style>
